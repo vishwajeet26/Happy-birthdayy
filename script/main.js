1c9be3711904af4f42ae94f3d54eb4b7abc7a0cc
vishwajeet singh <vishwajeet@vishwajeets-MacBook-Air.local>
2025-05-03 11:51:51 +0530

    changed warning message

--- script/main.js
+++ script/main.js
@@ -1,9 +1,9 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
     Swal.fire({
-        title: 'Do you want to play music in the background?',
+        title: 'Hi Madam, wanna play your fav music??',
         icon: 'warning',
-        showCancelButton: true,
+        showCancelButton: false,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
         confirmButtonText: 'Yes',
